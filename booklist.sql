2484128503cd36d7d6597860f790652b8b63f364
banburi <noonsrcmaru@gmail.com>
2023-10-16 16:32:31 +0900

    Changes

--- booklist.sql
+++ booklist.sql
@@ -1,22 +1,35 @@
-CREATE DATABASE IF NOT EXISTS list;
+CREATE DATABASE IF NOT EXISTS list1;
 -- 데이터베이스 연결 설정
-CREATE DATABASE IF NOT EXISTS list;
-USE list;
+CREATE DATABASE IF NOT EXISTS list1;
+USE list1;
 
 -- 도서 정보 테이블 생성
 CREATE TABLE IF NOT EXISTS `book` (
                                     id INT AUTO_INCREMENT PRIMARY KEY,
+                                    num varchar(255) NOT NULL,
                                     name VARCHAR(255) NOT NULL,
                                     author VARCHAR(255) NOT NULL,
                                     publisher VARCHAR(255) NOT NULL,
-                                    code VARCHAR(255) NOT NULL
+                                    publication_date varchar(255) NOT NULL,
+                                    code VARCHAR(255) NOT NULL,
+                                    registration_number VARCHAR(255) NOT NULL,
+                                    material_status VARCHAR(255) NOT NULL
 );
 
 -- 도서 정보 삽입
-INSERT INTO `book` (name, author, publisher, code) VALUES
-                                         ('책 제목 1', '저자 1', '출판사 1', '기호 1'),
-                                         ('책 제목 2', '저자 2', '출판사 2', '기호 2'),
-                                         ('책 제목 3', '저자 3', '출판사 3', '기호 3');
+INSERT INTO `book` (num, name, author, publisher, publication_date,
+                    code, registration_number, material_status) VALUES
+                     ('231a', '책 제목 1', '저자 1', '출판사 1', '1as', '기호 1', '등록번호', '자료상태'),
+                     ('423s', '책 제목 2', '저자 2', '출판사 2', '2df', '기호 2', '등록번호', '자료상태'),
+                     ('463s', '책 제목 4', '저자 4', '출판사 4', '4df', '기호 4', '등록번호', '자료상태');
+
+-- CSV 파일로부터 도서 정보 삽입
+/*OAD DATA LOCAL INFILE '""C:/Users/seojun/Desktop/test_table.csv""'
+    INTO TABLE list.`book`
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS;*/
 
 -- 도서 정보 조회
 SELECT * FROM `book`;
--- index.php
+++ index.php
@@ -1,71 +1,79 @@
-<?php
-// 데이터베이스 연결 설정
-$host = "localhost"; // 데이터베이스 호스트
-$username = "root"; // 데이터베이스 사용자명
-$password = "2718"; // 데이터베이스 암호
-$database = "list"; // 데이터베이스 이름
-
-$conn = new mysqli($host, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("데이터베이스 연결 실패: " . $conn->connect_error);
-}
-
-// 검색어 가져오기
-if (isset($_GET['search'])) {
-    $search = $_GET['search'];
-
-    // SQL 쿼리 작성
-    $sql = "SELECT * FROM book WHERE name LIKE '%$search%'";
-
-    // 쿼리 실행
-    $result = $conn->query($sql);
-
-    // 결과 출력
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "제목: " . $row["name"] . "<br>";
-            echo "저자: " . $row["author"] . "<br>";
-            echo "출판 연도: " . $row["publisher"] . "<br>";
-            echo "ISBN: " . $row["code  "] . "<br><br>";
-        }
-    } else {
-        echo "검색 결과가 없습니다.";
-    }
-}
-
-// 데이터베이스 연결 닫기
-$conn->close();
-?>
-
 <!DOCTYPE html>
 <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <title>PenielBook</title>
-        <link href="style.css" rel="stylesheet" type="text/css"/>
-    </head>
-    <body>
-    <div id="wrap">
-        <div id="header" role="banner">
-            <div id="penielLogo">
-                <h1>PenielBook</h1>
-            </div>
-            <div id="search_area">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>PenielBook</title>
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div id="header">
+    <nav>
+        <div id="penielLogo">
+            <a class="navbar" href="https://www.google.com/"
+               style="color: #00b74a; font-size: 30px; font-weight: bold; margin-left: 15%;">PenielBook</a>
+            <div>
             <form method="GET" action="">
                 <label>
-                    <input type="text" name="search" placeholder="도서 이름을 입력하세요">
+                    <input type="text" class="searchbar" name="search" placeholder="도서 이름을 입력하세요">
                 </label>
-                <input type="submit" value="검색">
+                <input type="submit" class="search" value="검색">
             </form>
             </div>
         </div>
-        <div id="container">
-            <p></p>
-        </div>
-        <div id="footer">
-            <p></p>
-        </div>
-    </body>
+    </nav>
+</div>
+
+<!-- Page Content-->
+
+<div id="container">
+    <?php
+    // 데이터베이스 연결 설정
+    $host = "localhost";
+    $username = "root";
+    $password = "2718";
+    $database = "list1";
+
+    $conn = new mysqli($host, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("데이터베이스 연결 실패: " . $conn->connect_error);
+    }
+
+    // 검색어 가져오기
+    if (isset($_GET['search'])) {
+        $search = $_GET['search'];
+
+        // SQL 쿼리 작성
+        $sql = "SELECT * FROM book WHERE name LIKE '%$search%'";
+
+        // 쿼리 실행
+        $result = $conn->query($sql);
+
+        // 결과 출력
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "제목: " . $row["name"] . "<br>";
+                echo "저자: " . $row["author"] . "<br>";
+                echo "출판사: " . $row["publisher"] . "<br>";
+                echo "ISBN: " . $row["code"] . "<br><br>";
+                //break;
+            }
+
+        } else {
+            echo "검색 결과가 없습니다.";
+        }
+    }
+
+    // 데이터베이스 연결 닫기
+    $conn->close();
+    ?>
+</div>
+
+<!-- Footer-->
+
+<div id="footer">
+    <p></p>
+</div>
+</body>
 </html>
